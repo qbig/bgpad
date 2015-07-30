02e717545405ed95b4617816814672265234a0ca
Qiao Liang <qiaoliang89@gmail.com>
2015-07-30 14:22:49 +0800

    interactive only mainSelect

---
+++ BigSpoonPad/BigSpoonPad/MainGroupSelectCell.swift
@@ -0,0 +1,15 @@
+//
+//  MainGroupSelectCell.swift
+//  BigSpoonPad
+//
+//  Created by Qiao Liang on 30/7/15.
+//  Copyright (c) 2015 Qiao Liang. All rights reserved.
+//
+
+import UIKit
+
+class MainGroupSelectCell: UICollectionViewCell {
+    @IBOutlet weak var GroupOptionImage: UIImageView!
+    @IBOutlet weak var groupTitle: UILabel!
+    @IBOutlet weak var groupDescription: UILabel!
+}
--- BigSpoonPad/BigSpoonPad/MainGroupSelectVC.swift
+++ BigSpoonPad/BigSpoonPad/MainGroupSelectVC.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+let reuseIdentifier = "mainGroupSelect"
+let numOfSections = 1
+let numOfOptions = 8
+class MainGroupSelectVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+    let sectionInsets = UIEdgeInsets(top: 10.0, left: 5.0, bottom: 10.0, right: 5.0)
+    let titles = ["Sand Harbor, Lake Tahoe - California","Beautiful View of Manhattan skyline.","Watcher in the Fog","Great Smoky Mountains National Park, Tennessee","Most beautiful place"]
 
-class MainGroupSelectVC: UIViewController {
-    @IBOutlet weak var groupSelectScrollView: UIScrollView!
-    @IBOutlet weak var firstItem: UIImageView!
-    @IBOutlet weak var lastItem: UIImageView!
+    
+    @IBOutlet weak var groupSelectCollectionView: UICollectionView!
+    @IBOutlet weak var firstItem: MainGroupSelectCell!
     override func viewDidLoad() {
         super.viewDidLoad()
- 
+        self.groupSelectCollectionView.backgroundColor = UIColor.clearColor();
     }
    
     
@@ -28,18 +33,58 @@ class MainGroupSelectVC: UIViewController {
 
 
     @IBOutlet weak var scrollBackButton: UIButton!
-    
     @IBAction func scrollBackPressed() {
-        groupSelectScrollView.scrollRectToVisible(firstItem.frame, animated: true)
+        var frame = self.firstItem.frame
+        frame.origin.x = 0
+        self.groupSelectCollectionView.scrollRectToVisible(frame, animated: true)
     }
     
     @IBOutlet weak var scrollNextButton: UIButton!
-    
     @IBAction func scrollNextPressed() {
-        groupSelectScrollView.scrollRectToVisible(lastItem.frame, animated: true)
+        var frame = self.firstItem.frame
+        frame.origin.x = frame.origin.x + 170 * 7
+        self.groupSelectCollectionView.scrollRectToVisible(frame, animated: true)
     }
     
-    @IBAction func groupSelected(sender: AnyObject) {
-        println("selected")
+    // MARK: UICollectionViewDataSource
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return numOfSections
     }
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numOfOptions
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! MainGroupSelectCell
+        cell.groupTitle.text = self.titles[indexPath.row % 5]
+        let curr = indexPath.row % 5  + 1
+        let imgName = "pin\(curr).jpg"
+        cell.GroupOptionImage.image = UIImage(named: imgName)
+        if indexPath.item == 0 {
+            self.firstItem = cell
+        }
+        return cell
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+            return CGSize(width: 160, height: 320)
+    }
+    
+    func collectionView(collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+            return sectionInsets
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath){
+        performSegueWithIdentifier("MainToItem", sender: nil);
+    }
+    
 }
